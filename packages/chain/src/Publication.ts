6dc2f68ab1400b49b6d180ed33b8ad22ff8ef9c1
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2024-05-18 17:04:47 +0200

    organized some structure

--- packages/chain/src/Publication.ts
+++ packages/chain/src/Publication.ts
@@ -1,71 +1,10 @@
 import { Bool, CircuitString, Experimental, Field, MerkleMapWitness, MerkleTree, Nullifier, Poseidon, Struct, UInt64 } from "o1js";
 import {
     RuntimeModule,
-    runtimeMethod,
     state,
     runtimeModule,
 } from "@proto-kit/module";
-import { State, StateMap } from "@proto-kit/protocol";
-import { UInt32 } from "@proto-kit/library";
-
-
-// export const id = Experimental.ZkProgram({
-//     publicOutput: {
-//         hasValidEmail: Bool,
-//     },
-//     methods: {
-//       canClaim: {
-//         privateInputs: [MerkleMapWitness, Nullifier],
-//         method: canClaim,
-//       },
-//     },
-//   });
-
-// export class IdProof extends Experimental.ZkProgram.Proof();
-
-
-
-// PublicationPublicOutput
-export class PublicationPublicOutput extends Struct({
-    root: Field,
-    nullifier: Field,
-}) { }
-
-// canPublish function for the publish circuit
-// private inputs: proof of identity (zkemail or something else) - this is gonna be abstracted for now
-export function canPublish(witness: MerkleMapWitness, nullifier: Nullifier): PublicationPublicOutput {
-    // verify identity proof
-
-
-    const key = Poseidon.hash(nullifier.getPublicKey().toFields());
-    const [computedRoot, computedKey] = witness.computeRootAndKey(
-        Bool(true).toField()
-    );
-
-    // has reputation already? maybe here is an if
-    // if this assertion is ok, otherwise we will just update the merkle tree with its key
-    computedKey.assertEquals(key);
-
-
-    // returns the publication 
-    return new PublicationPublicOutput({
-        root: computedRoot,
-        nullifier: key,
-    });
-}
-
-export const publishCircuit = Experimental.ZkProgram({
-    // publicInput: idProof,
-    publicOutput: PublicationPublicOutput,
-    methods: {
-        canPublish: {
-            privateInputs: [MerkleMapWitness, Nullifier],
-            method: canPublish,
-        },
-    },
-});
-
-export class PublishProof extends Experimental.ZkProgram.Proof(publishCircuit) { }
+import { StateMap } from "@proto-kit/protocol";
 
 // TODO: check this
 type PublicationsConfig = Record<string, never>;
@@ -74,7 +13,7 @@ export class Publication extends Struct({
     content: CircuitString,
     timestamp: UInt64,
     score: UInt64
-}) {}
+}) { }
 
 @runtimeModule()
 export class Publications extends RuntimeModule<PublicationsConfig> {
@@ -83,5 +22,5 @@ export class Publications extends RuntimeModule<PublicationsConfig> {
         Publication
     );
 
-    publish(publishProof: PublishProof) { }
+    add() {}
 }
--- packages/chain/src/ZKPeer.ts
+++ packages/chain/src/ZKPeer.ts
@@ -1,9 +1,58 @@
 import { RuntimeModule, runtimeMethod, state } from "@proto-kit/module";
-import { State, StateMap } from "@proto-kit/protocol";
-import { Bool, Field, UInt64 } from "o1js";
+import { State, StateMap, assert } from "@proto-kit/protocol";
+import { Bool, Experimental, Field, MerkleMapWitness, Nullifier, Poseidon, Struct, UInt64 } from "o1js";
 import { Publications } from "./Publication";
 import { Balances } from "@proto-kit/library";
 import { inject } from 'tsyringe';
+import { Reputation } from "./Reputation";
+
+///////////////////////////////////////////////////////
+////////////////// Publication Proof //////////////////
+///////////////////////////////////////////////////////
+export class PublicationPublicOutput extends Struct({
+    root: Field,
+    nullifier: Field,
+}) { }
+
+// canPublish function for the publish circuit
+// private inputs: proof of identity (zkemail or something else) - this is gonna be abstracted for now
+export function canPublish(witness: MerkleMapWitness, nullifier: Nullifier): PublicationPublicOutput {
+    // verify identity proof
+
+
+    const key = Poseidon.hash(nullifier.getPublicKey().toFields());
+    const [computedRoot, computedKey] = witness.computeRootAndKey(
+        Bool(true).toField()
+    );
+
+    // has reputation already? maybe here is an if
+    // if this assertion is ok, otherwise we will just update the merkle tree with its key
+    computedKey.assertEquals(key);
+
+
+    // returns the publication 
+    return new PublicationPublicOutput({
+        root: computedRoot,
+        nullifier: key,
+    });
+}
+
+export const publishCircuit = Experimental.ZkProgram({
+    // publicInput: idProof,
+    publicOutput: PublicationPublicOutput,
+    methods: {
+        canPublish: {
+            privateInputs: [MerkleMapWitness, Nullifier],
+            method: canPublish,
+        },
+    },
+});
+
+export class PublishProof extends Experimental.ZkProgram.Proof(publishCircuit) { }
+
+///////////////////////////////////////////////////////
+////////////////// Review Proof //////////////////
+///////////////////////////////////////////////////////
 
 type ZKPeerConfig = Record<string, never>;
 
@@ -12,7 +61,11 @@ export class ZKPeer extends RuntimeModule<ZKPeerConfig> {
     // the nullifier will map the pseudo-user to the root of his hash tree of publications
     @state() public nullifiers = StateMap.from<Field, UInt64>(Field, UInt64);
 
-    public constructor(@inject("Balances") private balances: Balances) {
+    public constructor(
+        @inject("Balances") private balances: Balances,
+        @inject("Publications") private publications: Publications,
+        @inject("Reputation") private reputation: Reputation
+    ) {
         super();
     }
 
@@ -22,8 +75,19 @@ export class ZKPeer extends RuntimeModule<ZKPeerConfig> {
     }
 
     @runtimeMethod()
-    public publish() {}
+    public publish(publishProof: PublishProof) {
+        publishProof.verify();
+        const commitment = this.commitment.get();
+
+        assert(
+            publishProof.publicOutput.root.equals(commitment.value),
+            "Publish proof does not contain the correct commitment"
+        );
+
+
+        this.publications.add()
+    }
 
     @runtimeMethod()
-    public review() {}
+    public review() { }
 }
